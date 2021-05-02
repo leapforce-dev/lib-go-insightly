9f9673f2c73ea204114f56e6b54b0c51235dfafd
caselongo <caselongo@gmail.com>
2021-05-02 21:25:52 +0200

    x

--- Instance.go
+++ Instance.go
@@ -1,6 +1,8 @@
 package insightly
 
 import (
+	"net/http"
+
 	errortools "github.com/leapforce-libraries/go_errortools"
 	go_http "github.com/leapforce-libraries/go_http"
 )
@@ -17,17 +19,17 @@ type Instance struct {
 // GetInstance returns all instance
 //
 
-func (service *Service) GetInstance() (*Instance, *errortools.Error) {
+func (service *Service) GetInstance() (*Instance, *http.Response, *errortools.Error) {
 	instance := Instance{}
 	requestConfig := go_http.RequestConfig{
 		URL:           service.url("Instance"),
 		ResponseModel: &instance,
 	}
 
-	_, _, e := service.get(&requestConfig)
+	_, response, e := service.get(&requestConfig)
 	if e != nil {
-		return nil, e
+		return nil, response, e
 	}
 
-	return &instance, nil
+	return &instance, response, nil
 }

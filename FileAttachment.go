6f38931738ee46be5ba322c38462d922f4ec1d49
caselongo <caselongo@gmail.com>
2023-06-14 21:59:00 +0200

    Update FileAttachment.go

--- FileAttachment.go
+++ FileAttachment.go
@@ -1,7 +1,12 @@
 package insightly
 
 import (
+	"fmt"
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
 	i_types "github.com/leapforce-libraries/go_insightly/types"
+	"io"
+	"net/http"
 )
 
 // FileAttachment stores FileAttachment from Service
@@ -15,4 +20,24 @@ type FileAttachment struct {
 	DateCreatedUtc *i_types.DateTimeString `json:"DATE_CREATED_UTC"`
 	DateUpdatedUtc *i_types.DateTimeString `json:"DATE_UPDATED_UTC"`
 	Url            string                  `json:"URL"`
+}
+
+// GetFileAttachment returns a specific file attachments as a slice of bytes
+func (service *Service) GetFileAttachment(fileId int64) ([]byte, *errortools.Error) {
+	requestConfig := go_http.RequestConfig{
+		Method: http.MethodGet,
+		Url:    service.url(fmt.Sprintf("fileattachments/%v", fileId)),
+	}
+
+	_, response, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errortools.ErrorMessage(err)
+	}
+
+	return b, nil
 }
